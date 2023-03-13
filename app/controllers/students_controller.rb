8b9c7d5b816a85913c21c170ce023f1d6fc0b420
David <david.owuor@student.moringaschool.com>
2023-03-13 22:56:33 +0300

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,8 +6,12 @@ class StudentsController < ApplicationController
   end
 
   def grades
-    students = Student.all
-   sorted = students.sort {|s,x| x.grade <=> s.grade}
-   render json: (sorted)
+  #   students = Student.all
+  #  sorted = students.sort {|s,x| x.grade <=> s.grade}
+  #  render json: (sorted)
+   render json: Student.all.sort {|x,y| y.grade<=>x.grade}
+  end
+  def highest_grade
+    render json: Student.all.sort {|x,y| y.grade<=>x.grade}.first
   end
 end
